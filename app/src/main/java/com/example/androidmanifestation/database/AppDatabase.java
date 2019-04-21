036e1998d312e3bf17d558969591385d6ae46dfc
disha4mourya <disha4mourya@gmail.com>
2019-04-21 14:44:43 +0530

    created room database with type converter for date data type

---
+++ app/src/main/java/com/example/androidmanifestation/database/AppDatabase.java
@@ -0,0 +1,30 @@
+package com.example.androidmanifestation.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+@Database(entities = {TaskEntity.class},version = 1,exportSchema = false)
+@TypeConverters(DateConverter.class)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG =AppDatabase.class.getSimpleName();
+
+    private static final String DATABASE_NAME="todolist";
+    private static  AppDatabase sInstance ;
+
+    public static AppDatabase getsInstance(Context context){
+        if (sInstance!=null){
+            sInstance= Room.databaseBuilder(context.getApplicationContext(),
+                    AppDatabase.class,
+                    AppDatabase.DATABASE_NAME)
+                    .build();
+        }
+        return sInstance;
+    }
+
+    public abstract TaskDao taskDao();
+
+}
