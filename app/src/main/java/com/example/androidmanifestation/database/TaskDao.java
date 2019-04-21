4b28ad4e535ccdc89b733de9462d4654dcc1a80a
disha4mourya <disha4mourya@gmail.com>
2019-04-21 14:11:51 +0530

    Added Data Access Object for querying database using Room.

---
+++ app/src/main/java/com/example/androidmanifestation/database/TaskDao.java
@@ -0,0 +1,30 @@
+package com.example.androidmanifestation.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+
+    @Query("SELECT * FROM task ORDER BY priority")
+    List<TaskEntity> loadAllTasks();
+
+    @Insert
+    void insertTask(TaskEntity taskEntity);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateTask(TaskEntity taskEntity);
+
+    @Delete
+    void deleteTask(TaskEntity taskEntity);
+
+    @Query("SELECT * FROM task WHERE id=:id")
+    TaskEntity loadTaskById(int id);
+
+}
