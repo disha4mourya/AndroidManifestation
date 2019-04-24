a3cd8ffe5f96ce1db2745b8acb745292c209fb8a
disham <disha4mourya@gmail.com>
2019-04-24 18:35:17 +0530

    Server call type calling activity added[IP]

--- app/src/main/java/com/example/androidmanifestation/ServerCallOptions.java
+++ app/src/main/java/com/example/androidmanifestation/ServerCallOptions.java
@@ -1,13 +1,41 @@
 package com.example.androidmanifestation;
 
+import android.databinding.DataBinderMapper;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class ServerCallOptions extends AppCompatActivity {
+import com.example.androidmanifestation.databinding.ActivityServerCallOptionsBinding;
+
+public class ServerCallOptions extends AppCompatActivity implements View.OnClickListener {
+
+    ActivityServerCallOptionsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_server_call_options);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_server_call_options);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnNoLibrary: {
+                break;
+            }
+            case R.id.btnLoopj: {
+                break;
+            }
+            case R.id.btnVolley: {
+                break;
+            }
+            case R.id.btnOkhttp: {
+                break;
+            }
+            case R.id.btnRetrofit: {
+                break;
+            }
+        }
     }
 }
