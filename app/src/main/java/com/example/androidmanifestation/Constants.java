a1f26d4d1cc325eebf406b3acee27cdd9515e414
disham <disha4mourya@gmail.com>
2019-04-28 11:09:42 +0530

    Server call without any library added

---
+++ app/src/main/java/com/example/androidmanifestation/Constants.java
@@ -0,0 +1,7 @@
+package com.example.androidmanifestation;
+
+public class Constants {
+    public static final String SONGS_LIST_URL = "https://itunes.apple.com/search?term=Michael+jackson";
+    public static final int CONNECTION_TIMEOUT = 10000;
+    public static final int READ_TIMEOUT = 15000;
+}
--- app/src/main/java/com/example/androidmanifestation/ServerCallOptions.java
+++ app/src/main/java/com/example/androidmanifestation/ServerCallOptions.java
@@ -1,12 +1,13 @@
 package com.example.androidmanifestation;
 
-import android.databinding.DataBinderMapper;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.androidmanifestation.databinding.ActivityServerCallOptionsBinding;
+import com.example.androidmanifestation.server_calls.NoLibraryCallActivity;
 
 public class ServerCallOptions extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,12 +17,20 @@ public class ServerCallOptions extends AppCompatActivity implements View.OnClick
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_server_call_options);
+
+        binding.btnNoLibrary.setOnClickListener(this);
+        binding.btnLoopj.setOnClickListener(this);
+        binding.btnVolley.setOnClickListener(this);
+        binding.btnOkhttp.setOnClickListener(this);
+        binding.btnRetrofit.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnNoLibrary: {
+                Intent intent = new Intent(this, NoLibraryCallActivity.class);
+                startActivity(intent);
                 break;
             }
             case R.id.btnLoopj: {
